f671ade743100aeaffecd7a4649d4ff42a9d26b8
avouraki <avouraki@uci.edu>
2017-07-12 21:08:58 -0700

    Contains JSON parser only

--- read_data.py
+++ read_data.py
@@ -15,29 +15,20 @@ A typical pipeline for reading records from files has the following stages:
 
 '''
 
+import json
+import numpy as np
+import os
 import tensorflow as tf
 
-filename_queue = tf.train.string_input_producer(["comments.csv"])
+def print_comments_from_json(file_path):
+    with open(file_path) as file:
+        data = json.load(file) #deserialize to python object
+        for comment in data:
+            print(comment["commentText"])
 
-reader = tf.TextLineReader()
-key, value = reader.read(filename_queue)
-
-# Default values, in case of empty columns. Also specifies the type of the
-# decoded result.
-record_defaults = [[1], [1], [1], [1], [1]]
-col1, col2, col3, col4, col5 = tf.decode_csv(
-    value, record_defaults=record_defaults)
-features = tf.stack([col1, col2, col3, col4])
-
-with tf.Session() as sess:
-  # Start populating the filename queue.
-  coord = tf.train.Coordinator()
-  threads = tf.train.start_queue_runners(coord=coord)
-
-  for i in range(1200):
-    # Retrieve a single instance:
-    example, label = sess.run([features, col5])
-
-  coord.request_stop()
-  coord.join(threads)
+if __name__ == '__main__':
+    directory = '/home/s4x5/Documents/github/yt-comments-topics'
+    file_name = 'comments_5.json'
+    file_path = os.path.join(directory, file_name)
+    print_comments_from_json(file_path) 
 
